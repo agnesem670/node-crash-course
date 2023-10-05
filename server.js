a638cebfce0ccee420f906e0bcd29b32bcf38287
agnesem670 <agnesem670@gmail.com>
2023-10-05 11:50:42 +0200

    server request and response object

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+    // set header content type
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>hello world</p>')
+    res.end()
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on port 3000')
+})
