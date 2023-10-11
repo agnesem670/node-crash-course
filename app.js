1c127da4a5ddbd8545c61cbd9a10a58d931731d0
agnesem670 <agnesem670@gmail.com>
2023-10-11 10:05:26 +0200

    ejs - passing data into views

--- app.js
+++ app.js
@@ -10,19 +10,25 @@ app.set('view engine', 'ejs')
 // listen for request
 app.listen(3000)
 
+
 app.get('/', (req, res) => {
-    res.render('index')
+    const blogs = [
+        {title: 'Title number 1', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+        {title: 'Title number 2', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+        {title: 'Title number 3', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+    ];
+    res.render('index', { title: "Home", blogs: blogs})
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', { title: "About"})
 })
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create')
+    res.render('create', { title: "Create a new blog"})
 })
 
 // 404 page 
 app.use((req, res) => {
-    res.status(404).render('404')
+    res.status(404).render('404', { title: "404"})
 })
