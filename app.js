fd1e503e9c1b9c8964aa770e011a77afe9f19c0b
agnesem670 <agnesem670@gmail.com>
2023-10-12 14:47:20 +0200

    mongoose

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 // express app
 const app = express()
 
 // connect to mongo db
-const mongoURI = 'mongodb+srv://agnestest:1234test@cluster0.kn9jvkp.mongodb.net/?retryWrites=true&w=majority'
-
+const dbURI = 'mongodb+srv://agnes670test:1234670test@cluster0.kn9jvkp.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
 // register view engine
 app.set('view engine', 'ejs')
 //app.set('views', 'folder_name') // only when folder name with views is different then "views"
-
-// listen for request
-app.listen(3000)
 
 // middleware and static files
 app.use(express.static('public'))
